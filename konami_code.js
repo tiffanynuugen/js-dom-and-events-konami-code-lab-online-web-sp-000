f1f523cdb7401db35ccd6ce312091b0acf7e944a
tiffanynuugen <tiffanynuugen@gmail.com>
2019-02-14 22:31:30 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,18 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let i = 0
 
+function init() {
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+      if (key === code[i]) {
+        i++;
+      if (i === code.length) {
+        alert("Congratz, you did it!")
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  })
 }
